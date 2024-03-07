c2e321aee19e187e782b1ac5e91bc2a8a85f5f2c
Leonardo Kagohara <leonardo.kagohara@starkbank.com>
2024-03-07 14:48:47 -0300

    Add update method to issuingPurchase

--- sdk/issuingPurchase/index.js
+++ sdk/issuingPurchase/index.js
@@ -4,6 +4,7 @@ exports.log = require('./log');
 exports.get = issuingPurchase.get;
 exports.query = issuingPurchase.query;
 exports.page = issuingPurchase.page;
+exports.update = issuingPurchase.update;
 exports.parse = issuingPurchase.parse;
 exports.response = issuingPurchase.response;
 exports.IssuingPurchase = issuingPurchase.IssuingPurchase;
